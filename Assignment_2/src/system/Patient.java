35b090f174741b49f98898411fad9d241ed86fb6
Aastha Prashar <prashar.a@northeastern.edu>
2022-10-23 18:30:52 -0400

    adding Patient

---
+++ Assignment_2/src/system/Patient.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package system;
+
+/**
+ *
+ * @author parth
+ */
+public class Patient {
+     public int patId;
+    public String firstName;
+    public String lastName;
+    public String patPhone;
+    public int age; 
+    public String gender;
+    public String dob;
+    public int height;
+    public int weight;
+    public String house;
+
+    public int getPatId() {
+        return patId;
+    }
+
+    public void setPatId(int patId) {
+        this.patId = patId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPatPhone() {
+        return patPhone;
+    }
+
+    public void setPatPhone(String patPhone) {
+        this.patPhone = patPhone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+    
+    
+}
